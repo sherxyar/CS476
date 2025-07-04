aa406a3d3b4582d9c99961ef5774728fc0a23e35
Mohammad Sheryar <66784687+sherxyar@users.noreply.github.com>
2025-07-03 21:49:14 -0600

    0703 - sheryar - included mutate in hook

--- cpms/src/lib/invoices.ts
+++ cpms/src/lib/invoices.ts
@@ -1,14 +1,18 @@
-// src/lib/invoices.ts
 import useSWR from "swr";
 import type { Invoice } from "@prisma/client";
 
 export default function useInvoices(projectId: string) {
   const fetcher = (url: string) => fetch(url).then(r => r.json());
 
-  const { data, error, isLoading } = useSWR<Invoice[]>(
+  const { data, error, isLoading, mutate } = useSWR<Invoice[]>(
     `/api/projects/${projectId}/invoices`,
     fetcher,
   );
 
-  return { invoices: data, isLoading, error };
+  return {
+    invoices: data,
+    isLoading,
+    error,
+    mutate,            
+  };
 }
