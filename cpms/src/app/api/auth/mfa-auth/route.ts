79e608bcd56c327e1c124ccfdb9bf3c2fa0ff98f
Ishan Soni <ishansoni.work@gmail.com>
2025-07-04 22:31:09 -0600

    Created api route for 2FA. Cleaned prisma file and login route file.

---
+++ cpms/src/app/api/auth/mfa-auth/route.ts
@@ -0,0 +1,15 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    const { email, code } = await req.json()
+    const user = await prisma.user.findUnique({ where: { email }, include: { mfaCodes: true } });
+    if (!user) {
+        return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+    const isValidCode = user.mfaCodes.find((current_session: { code: string; validUntil: Date }) => current_session.code === code && new Date(current_session.validUntil) > new Date());
+    if (!isValidCode) {
+        return NextResponse.json({ error: "Code is invalid or expired" }, { status: 403 });
+    }
+    return NextResponse.json({ message: "Multi-factor authentication successful", user: { id: user.id, role: user.role } });
+}
